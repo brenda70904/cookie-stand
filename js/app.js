0221d5cfe6ebbe5d895ef77b97be914ad9600103
brenda70904 <brendaj0214@gmail.com>
2022-10-11 22:10:18 -0700

    complete table

--- js/app.js
+++ js/app.js
@@ -41,7 +41,7 @@ CookiesStands.prototype.generateCookieSale = function () {
   for (let i = 0; i < hours.length; i++) {
     let cookiesThisHr = Math.ceil(randomCust(this) * this.avgSale);
     this.cookieSoldperHrs.push(cookiesThisHr);
-  }
+  }// cookieThisHr array
 };
 
 //add up all cookie sale per hour , add to td tag with total
@@ -70,8 +70,43 @@ let tokyo = new CookiesStands('Tokyo', 3, 24, 1.2);
 let dubai = new CookiesStands('Dubai', 11, 38, 2.7);
 let paris = new CookiesStands('Paris', 3, 24, 1.2);
 let lima = new CookiesStands('Lima', 3, 24, 1.2);
+let cities = [seattle, tokyo, dubai, paris, lima];
 seattle.cookiesSaleTotal();
 tokyo.cookiesSaleTotal();
 dubai.cookiesSaleTotal();
 paris.cookiesSaleTotal();
 lima.cookiesSaleTotal();
+console.log(seattle.generateCookieSale());
+
+function footer() {
+  let footerRow = document.createElement('tr');
+  let tablefooter = document.createElement('td');
+  tablefooter.textContent = 'Hourly Total';
+  table.appendChild(footerRow);
+  footerRow.appendChild(tablefooter);
+
+
+  let hourTotalList = [];
+  for (let i = 0; i < hours.length; i++) {
+    let sum = 0;
+    for (let j = 0; j < cities.length; j++) {
+      //add each citiy's sale total
+      sum += cities[j].cookieSoldperHrs[i];
+    }
+    hourTotalList.push(sum);
+    // add hourTotal into arry
+    let td = document.createElement('td');
+    td.textContent = hourTotalList[i];
+    footerRow.appendChild(td);
+  }
+  let td = document.createElement('td');
+  td.textContent = seattle.dailyTotal+tokyo.dailyTotal+dubai.dailyTotal+paris.dailyTotal+lima.dailyTotal;
+  footerRow.appendChild(td);
+}
+footer();
+
+
+
+//1st for loop : 把個城市的sale家再一起 cookieSold ++
+//2nd forloop : 找出每個城市each hour[i]的sale
+//vgSale * randomCust(cookiestands);
